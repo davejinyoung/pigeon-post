2d6816151c3ba095a5f163d4a75ecb9088044df7
davejinyoung <davejinyoung@gmail.com>
2025-02-17 00:10:59 -0800

    Creating more components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UnreadEmailsComponent } from './unread-emails/unread-emails.component';
+import { EmailSummariesComponent } from './email-summaries/email-summaries.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'emails', component: UnreadEmailsComponent },
+    { path: 'summaries', component: EmailSummariesComponent }
+  ];

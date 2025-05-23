2d401bbdd45a324b264cc1cfc88dd28ae9266fe8
davejinyoung <davejinyoung@gmail.com>
2025-05-22 22:57:20 -0700

    Removing unnecessary files

--- ollama_chat.py
+++
@@ -1,9 +0,0 @@
-import llama_cpp
-
-# Initialize LLaMA model
-llama = llama_cpp.Llama(model_path="path/to/your/llama-model")
-
-# Generate a response
-response = llama.generate("Your prompt here")
-
-print(response)

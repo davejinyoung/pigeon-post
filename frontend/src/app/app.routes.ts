92399c248ef0a367221e9c9500660988d5894e4f
davejinyoung <davejinyoung@gmail.com>
2025-05-25 20:29:17 -0700

    Initialized settings component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EmailsComponent } from './emails/emails.component';
 import { EmailSummariesComponent } from './email-summaries/email-summaries.component';
+import { SettingsComponent } from './settings/settings.component';
 
 export const routes: Routes = [
     { path: 'emails', component: EmailsComponent, title: 'Emails' },
     { path: 'summaries', component: EmailSummariesComponent, title: 'Email Summaries' },
     { path: 'summaries/saved', component: EmailSummariesComponent, title: 'Saved Email Summaries' },
-    { path: '', component: HomeComponent, title: 'Home' }
+    { path: '', component: HomeComponent, title: 'Home' },
+    { path: 'settings', component: SettingsComponent, title: 'Settings' },
   ];

9b8e34dd3b4f524b7b32fffc9f463d23bbf6eee9
davejinyoung <davejinyoung@gmail.com>
2025-05-31 12:27:02 -0700

    Login page initialization

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { HomeComponent } from './home/home.component';
 import { EmailsComponent } from './emails/emails.component';
 import { EmailSummariesComponent } from './email-summaries/email-summaries.component';
 import { SettingsComponent } from './settings/settings.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: 'emails', component: EmailsComponent, title: 'Emails' },
+    { path: 'login', component: LoginComponent, title: 'Login' },
     { path: 'summaries', component: EmailSummariesComponent, title: 'Email Summaries' },
     { path: 'summaries/saved', component: EmailSummariesComponent, title: 'Saved Email Summaries' },
     { path: '', component: HomeComponent, title: 'Home' },
